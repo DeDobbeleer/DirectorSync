6760b7174eb03e9781261f3050833197b3c5d545
Gaetan DE DOBBELEER <gaet.ddb@orange.fr>
2025-09-24 18:53:44 +0200

    .

--- lp_tenant_importer/core/http.py
+++ lp_tenant_importer/core/http.py
@@ -462,7 +462,9 @@ class DirectorClient:
                 "name": policy["name"],
                 "norm_policy": policy.get("norm_policy", ""),
                 "enrich_policy": policy.get("enrich_policy", "None"),
-                "routing_policy": policy.get("routing_policy", "None")
+                "routing_policy": policy.get("routing_policy", "None"),
+                "active": True,
+                "tid": "",
             }
         }
         logger.debug("Create request body for %s: %s", url, json.dumps(payload, indent=2))
@@ -493,7 +495,9 @@ class DirectorClient:
             "data": {
                 "norm_policy": policy.get("norm_policy", ""),
                 "enrich_policy": policy.get("enrich_policy", "None"),
-                "routing_policy": policy.get("routing_policy", "None")
+                "routing_policy": policy.get("routing_policy", "None"),
+                "active": True,
+                "tid": "",
             }
         }
         logger.debug("Update request body for %s: %s", url, json.dumps(payload, indent=2))

208828421b1b3c7f45cd22d42e448e34a464a532
Gaetan DE DOBBELEER <gaet.ddb@orange.fr>
2025-09-25 07:34:10 +0200

    .

--- lp_tenant_importer/core/http.py
+++ lp_tenant_importer/core/http.py
@@ -496,6 +496,7 @@ class DirectorClient:
         payload = {
             "data": {
                 "id": policy_id,
+                "policy_name": policy["name"],
                 "norm_policy": policy.get("norm_policy", ""),
                 "enrich_policy": policy.get("enrich_policy", "None"),
                 "routing_policy": policy.get("routing_policy", "None"),

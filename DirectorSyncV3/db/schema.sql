a75b2051b55a58f5773581adb88cbcf39a9615a4
Gaetan DE DOBBELEER <gaet.ddb@orange.fr>
2025-11-26 17:41:19 +0100

    .

--- DirectorSyncV3/db/schema.sql
+++
@@ -1,8 +0,0 @@
--- DirectorSync V3 database schema (draft placeholder).
-
-CREATE TABLE IF NOT EXISTS tenants (
-    id SERIAL PRIMARY KEY,
-    tenant_code TEXT NOT NULL UNIQUE
-);
-
--- Additional tables such as repos, routing_policies, alert_rules will follow.
